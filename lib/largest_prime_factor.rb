33e00f160089f332295270772a63aa01b50d7192
newtypeV2 <gianlibunao@gmail.com>
2019-08-13 10:18:20 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,28 @@
 # Enter your procedural solution here!
+def largest_prime_factor(number)
+
+    # while (number % 2 == 0)
+    #     number /= 2
+    # end
+    dividerlimit = Math.sqrt(number).round
+    start = 2
+    factors = []
+    while (start < dividerlimit )
+        if (number % start == 0 && is_prime?(start))
+            factors.push(start)
+        end
+        start += 1
+    end
+    binding.pry
+end
+
+def is_prime?(number)
+    div = 2
+    while(div < number)
+        if (number % div == 0)
+            return false
+        end
+        div += 1
+    end
+    return true
+end
